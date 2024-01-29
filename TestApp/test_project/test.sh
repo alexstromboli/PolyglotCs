8817f0e21404606d7f6350881c24a3e0333c7c11
Alexey Piskarev <alex.stromboli@gmail.com>
2024-01-29 05:13:43 -0500

    .NET 8

--- TestApp/test_project/test.sh
+++ TestApp/test_project/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -rf out flat
-../../Compile/bin/Debug/net5.0/Compile -out out -flatts flat
+../../Compile/bin/Debug/net8.0/Compile -out out -flatts flat
 find -type f | sort | xargs sha1sum >out/hash.txt
